548b5855e7e92dc1f11e60640781545548aaf79e
csilverman <chris@csilverman.com>
2020-12-10 17:08:20 -0500

    Added custom readmore

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 function custom_readmore() {
 	return '<p class="readmore"><a class="readmore__link" href="' . get_permalink() . '">Read More</a></p>';
 }
+
 add_filter( 'the_content_more_link', 'custom_readmore' );
